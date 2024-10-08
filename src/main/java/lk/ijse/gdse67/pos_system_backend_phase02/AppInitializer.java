b3c0a30eb6bcf229e25a55a19ada7a250130947a
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-10-08 20:59:22 +0530

    create config files and register AppInitializer class

--- src/main/java/lk/ijse/gdse67/pos_system_backend_phase02/AppInitializer.java
+++ src/main/java/lk/ijse/gdse67/pos_system_backend_phase02/AppInitializer.java
@@ -1,21 +1,23 @@
 package lk.ijse.gdse67.pos_system_backend_phase02;
 
+import lk.ijse.gdse67.pos_system_backend_phase02.config.WebAppConfig;
+import lk.ijse.gdse67.pos_system_backend_phase02.config.WebAppRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
---
+++ src/main/java/lk/ijse/gdse67/pos_system_backend_phase02/config/WebAppConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse67.pos_system_backend_phase02.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("lk.ijse.gdse67.pos_system_backend_phase02")
+public class WebAppConfig {
+}
