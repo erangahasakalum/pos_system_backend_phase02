0d1c8dc434351e542185a2809facea2466bd93ad
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-10-08 20:51:39 +0530

    add spring dependency ,controller package and AppInitializer class define

---
+++ src/main/java/lk/ijse/gdse67/pos_system_backend_phase02/controller/HealthCheckController.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse67.pos_system_backend_phase02.controller;
+
+
+public class HealthCheckController {
+    public String healthCheck(){
+        return "Server is running";
+    }
+}
+
