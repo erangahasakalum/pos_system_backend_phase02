60dbf44384d6adcbd07e048b482961d42a3fc88c
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-10-08 22:29:57 +0530

    check server status server is running

--- src/main/java/lk/ijse/gdse67/pos_system_backend_phase02/controller/HealthCheckController.java
+++ src/main/java/lk/ijse/gdse67/pos_system_backend_phase02/controller/HealthCheckController.java
@@ -1,7 +1,12 @@
 package lk.ijse.gdse67.pos_system_backend_phase02.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("api/v1/health")
 public class HealthCheckController {
+    @GetMapping
     public String healthCheck(){
         return "Server is running";
     }
