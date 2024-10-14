1969eb90828cf1e892a73e0e1459660b36b29fb0
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-10-14 21:03:39 +0530

    create itemException class

---
+++ src/main/java/lk/ijse/gdse67/pos_system_backend_phase02/exception/ItemNotPoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse67.pos_system_backend_phase02.exception;
+
+public class ItemNotPoundException extends Exception{
+    public ItemNotPoundException() {
+        super();
+    }
+
+    public ItemNotPoundException(String message) {
+        super(message);
+    }
+
+    public ItemNotPoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
